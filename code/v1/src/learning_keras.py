3a5e5c22091f995533c15f9432a854c23ffd9319
Aron <aron@Arons-MacBook-Pro.local>
2018-04-04 21:30:00 -0500

    no message

--- code/v1/src/learning_keras.py
+++ code/v1/src/learning_keras.py
@@ -30,9 +30,10 @@ class QLearning:
   States are represented as lists of arbitrary floats, while actions are represented as normalized lists of floats (i.e., floats are greater than or equal to zero and sum up to one).
   Differences compared to Q-learning are described in the documentation of the relevant functions.
   """
-  BEST_ACTION_ITERATIONS = 100 # small value because querying the neural network is very slow in the current implementation
-  LEARN_INITIAL_ITERATIONS = 131072
-  LEARN_ITERATIONS = 131072
+  BEST_ACTION_ITERATIONS = 10 # small value because querying the neural network is very slow in the current implementation
+  LEARN_INITIAL_ITERATIONS = 16384 # 131072
+  LEARN_ITERATIONS = 16384 # 131072
+  TRAIN_EPOCHS = 5
   DISCOUNT = 0.5
   
   def __init__(self, state_size, action_size):
@@ -65,6 +66,26 @@ class QLearning:
     for value in output:
       return value[0]
   
+  def Q_batch(self, state_action):
+    """
+    Compute the value of actions in given states.
+    :param state_action: List of tuples (state, action), where state is represented as a list of floats, action is represented as a normalized list of floats.
+    :return: List of values of the actions in the given states (i.e., Q(state, action)).
+    """
+    inputs = []
+    for (state, action) in state_action:
+      assert(len(state) == self.state_size)
+      assert(len(action) == self.action_size)
+      assert(abs(sum(action) - 1) < EPSILON)
+      assert(min(action) >= 0)
+      inputs.append(state + action)
+    output = self.regressor.predict(array(inputs))
+    values = []
+    for value in output:
+      values.append(value[0])
+    assert(len(state_action) == len(values))
+    return values
+  
   def best_action(self, state):
     """
     Finds the best action in a given state based on the action-value function (i.e., Q) and a simple hill-climbing heuristic. 
@@ -86,6 +107,37 @@ class QLearning:
     logging.debug("N: {}, action (unnormalized): {}".format(state, action))
     return normalized(action)
     
+  def best_action_batch(self, states):
+    """
+    Finds the best actions in given states based on the action-value function (i.e., Q) and a simple hill-climbing heuristic. 
+    Note that the value is minimized (instead of maximized).
+    :param states: List of states represented as lists of floats.
+    :return: List of tuples (action, value), where action is the best action, represented as a normalized list of floats, and value is the value of action in state (i.e., Q(state, action)).
+    """     
+    best_actions = [[1] * self.action_size for s in range(len(states))]
+    best_values = [float("inf") for s in range(len(states))]        
+    for i in range(QLearning.BEST_ACTION_ITERATIONS):  
+      logging.debug("best_action_batch (iteration: {})".format(i)) 
+      state_action = []
+      changed_actions = []  
+      for s in range(len(states)):        
+        for a in range(self.action_size):       
+          action = list(best_actions[s])
+          action[a] += 1        
+          state_action.append((states[s], normalized(action)))
+          changed_actions.append(action)        
+      values = self.Q_batch(state_action)
+      assert(len(values) == len(state_action))    
+      j = 0    
+      for s in range(len(states)):      
+        for a in range(self.action_size):      
+          if (values[j] < best_values[s]):
+            best_values[s] = values[j]
+            best_actions[s] = changed_actions[j]
+          j += 1         
+      assert(j == len(state_action))        
+    return [(normalized(best_actions[s]), best_values[s]) for s in range(len(states))]
+    
   def update(self, state_action_value):
     """
     Updates the state-value function based on the given combinations of states, actions, and values.
@@ -102,7 +154,7 @@ class QLearning:
       output_data.append(value)
     input_train = array(input_data, dtype=float32)
     output_train = array(output_data, dtype=float32)  
-    self.regressor.fit(input_train, output_train, epochs=10, batch_size=500)
+    self.regressor.fit(input_train, output_train, epochs=QLearning.TRAIN_EPOCHS, batch_size=512)
     
   def learn(self, initial_state, state_observe, state_update, rnd=Random(0)):
     """
@@ -117,7 +169,7 @@ class QLearning:
     state = initial_state
     state_action_value = []
     for i in range(QLearning.LEARN_INITIAL_ITERATIONS):
-      action = normalized([rnd.random() for i in range(self.action_size)])
+      action = normalized([rnd.random() for a in range(self.action_size)])
       (next_state, loss) = state_update(state, action)
       state_action_value.append((state_observe(state), action, loss))
       state = next_state
@@ -128,24 +180,34 @@ class QLearning:
     logging.info("Training duration: {}".format(duration))
     
     # temporary code for performance measurement
-    start_time = time()
-    for i in range(10000):
-      action = normalized([rnd.random() for i in range(self.action_size)])
-      state = normalized([rnd.random() for i in range(self.state_size)])
-      self.Q(state, action)
-    duration = time() - start_time
-    logging.info("Test duration: {}".format(duration))
-    
+#    start_time = time()
+#    state_action = []
+#    for i in range(10000):
+#      action = normalized([rnd.random() for i in range(self.action_size)])
+#      state = normalized([rnd.random() for i in range(self.state_size)])
+#      state_action.append((state, action))
+#      self.Q(state, action)
+#    self.Q_batch(state_action)
+#    duration = time() - start_time
+#    logging.info("Test duration: {}".format(duration))
+        
     # second phase (consider value of next state)
     state = initial_state
+    state_action_loss = []
+    next_states = []
+    for i in range(QLearning.LEARN_ITERATIONS):
+      action = normalized([rnd.random() for a in range(self.action_size)])
+      (next_state, loss) = state_update(state, action)
+      state_action_loss.append((state_observe(state), action, loss))
+      next_states.append(state_observe(next_state))
+      state = next_state
+    action_value = self.best_action_batch(next_states)
+    assert(len(action_value) == len(next_states))
     state_action_value = []
     for i in range(QLearning.LEARN_ITERATIONS):
-      action = normalized([rnd.random() for i in range(self.action_size)])
-      (next_state, loss) = state_update(state, action)
-      next_action = self.best_action(state_observe(next_state))
-      value = loss + QLearning.DISCOUNT * self.Q(state_observe(next_state), next_action)
-      state_action_value.append((state_observe(state), action, value))
-      state = next_state
+      (state, action, loss) = state_action_loss[i]
+      value = loss + QLearning.DISCOUNT * action_value[i][1]
+      state_action_value.append((state, action, value))
     self.update(state_action_value)
     logging.info("Second phase completed.")
 
