68511a786feda14717bd6df778167c82e09121f7
Liang Tong <liangtong39@gmail.com>
2018-05-28 11:17:49 -0500

    Update learning_ddpg.py
    1. Using less replay memory, from 25k->10k;
    2. Increasing the batch size to 100% of the replay memory;
    3. Add test process;
    4. Improving the efficiency of training;

--- code/v1/src/learning_ddpg.py
+++ code/v1/src/learning_ddpg.py
@@ -18,18 +18,24 @@ import tensorflow as tf
 import time
 import random
 import matplotlib.pyplot as plt
+import pickle
 #####################  hyper parameters  ####################
 
-MAX_EPISODES = 300
+MAX_EPISODES = 25
 MAX_EP_STEPS = 1000
 LR_A = 0.001    # learning rate for actor
 LR_C = 0.002    # learning rate for critic
 GAMMA = 0.9     # reward discount
 TAU = 0.01      # soft replacement
-MEMORY_CAPACITY = 25000
-BATCH_SIZE = int(0.3*MEMORY_CAPACITY) # only 30% data of the memory are seleted as mini-batch
+MEMORY_CAPACITY = 10000
+BATCH_SIZE = int(1.0*MEMORY_CAPACITY) # only 30% data of the memory are seleted as mini-batch
 
 RENDER = False
+
+
+TEST_EPISODES = 300
+MAX_TEST_STEPS = 1000
+
 
 def normalized(vect):
     """
@@ -116,9 +122,10 @@ class DDPG(object):
     def _build_a(self, s, reuse=None, custom_getter=None):
         trainable = True if reuse is None else False
         with tf.variable_scope('Actor', reuse=reuse, custom_getter=custom_getter):
-            h1 = tf.layers.dense(s, 32, activation=tf.nn.tanh, name='l1', trainable=trainable)
-            #h2 = tf.layers.dense(h1, 32, activation=tf.nn.relu, name='l2', trainable=trainable)
-            a = tf.layers.dense(h1, self.a_dim, activation=tf.nn.softmax, name='a', trainable=trainable)
+            h1 = tf.layers.dense(s, 64, activation=tf.nn.tanh, name='l1', trainable=trainable)
+            h2 = tf.layers.dense(h1, 32, activation=tf.nn.tanh, name='l2', trainable=trainable)
+            #h3 = tf.layers.dense(h2, 32, activation=tf.nn.tanh, name='l3', trainable=trainable)
+            a = tf.layers.dense(h2, self.a_dim, activation=tf.nn.softmax, name='a', trainable=trainable)
             return a
 
     def _build_c(self, s, a, reuse=None, custom_getter=None):
@@ -128,9 +135,10 @@ class DDPG(object):
             w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], trainable=trainable)
             w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], trainable=trainable)
             b1 = tf.get_variable('b1', [1, n_l1], trainable=trainable)
-            net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
-            net2 = tf.layers.dense(net, 32, activation=tf.nn.relu, trainable=trainable)
-            return tf.layers.dense(net, 1, trainable=trainable)  # Q(s,a)
+            net1 = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
+            net2 = tf.layers.dense(net1, 32, activation=tf.nn.relu, trainable=trainable)
+            #net3 = tf.layers.dense(net2, 32, activation=tf.nn.relu, trainable=trainable)
+            return tf.layers.dense(net2, 1, trainable=trainable)  # Q(s,a)
 
 class DDPGlearning:
     """
@@ -158,9 +166,9 @@ class DDPGlearning:
         :param state_update: Updates the state based on an action. Function, takes a state (see state_observe) and an action (normalized list of floats), return the next state (may be arbitrary object).
         :param rnd: Random number generator.
         """
-        logging.info("DDPG algorithm starts.")
-        # generate random states for state initilization in each eposide
-        
+        logging.info("DDPG training starts.")
+
+        # generate random states for state initilization in each eposide        
         states = []
         global_state = initial_state
         states.append(global_state)
@@ -168,54 +176,84 @@ class DDPGlearning:
             action = normalized([rnd.random() for i in range(self.action_size)])
             (next_global_state, loss) = state_update(global_state, action)
             states.append(next_global_state)
-        
+            global_state = next_global_state
+
         # ddpg training process
-        epsilon = 0.
+        epsilon = 0.0
         epsilon_max = 1.0
         t1 = time.time()
         rewards = []
-        for i in range(MAX_EPISODES):
-            #global_state = initial_state
+        mode_list = ["ddpg", "uniform_all", "uniform_youngest"]
+        mode = "ddpg"
+        if mode == "ddpg":
+            for i in range(MAX_EPISODES):
+                global_state = random.choice(states)
+                state = np.array(state_observe(global_state),dtype=np.float32)
+                total_reward = 0.0
+                exploit_reward = []
+                exploit_flag = True
+                for j in range(MAX_EP_STEPS):
+                    # epsilon-greedy
+                    if np.random.uniform() < epsilon:
+                        exploit_flag = True
+                        action = self.ddpg.choose_action(state) #action has been normalized by choos_action()
+                    else:
+                        exploit_flag = False
+                        action = normalized([rnd.random() for i in range(self.action_size)])
+                        action = np.array(action)
+                    
+                    (next_global_state, loss) = state_update(global_state, list(action))
+                    next_state = np.array(state_observe(next_global_state), dtype=np.float32)
+                    reward = -1.0*loss
+                    self.ddpg.store_transition(state, action, reward, next_state)
+
+                    if self.ddpg.pointer > MEMORY_CAPACITY and (j+1)%20 == 0:
+                        self.ddpg.learn()
+
+                    global_state = next_global_state
+                    state = next_state
+                    total_reward += reward
+                    if exploit_flag:
+                        exploit_reward.append(reward)
+
+                    if j == MAX_EP_STEPS-1:
+                        #print('Episode:', i, ' Reward: %i' % int(total_reward), 'Explore: %.5f' % epsilon, )
+                        ave_reward = total_reward/MAX_EP_STEPS
+                        #logging.info("Episode {}, Average reward in each step {}".format(i, ave_reward))
+                        if len(exploit_reward) == 0:
+                            logging.info("Episode {}, Ave reward {}, Ave exploitation reward unavailable".format(i, ave_reward))
+                        else:
+                            logging.info("Episode {}, Ave reward {}, Ave exploitation reward {}".format(i, ave_reward, sum(exploit_reward)/len(exploit_reward)))
+                        rewards.append(ave_reward)
+                        epsilon = epsilon+0.05 if epsilon < epsilon_max else epsilon_max
+                        break
+         
+            pickle.dump(rewards, open('pomdp.pickle','wb'))  
+        logging.info("DDPG test starts.")
+        total_reward = 0            
+        for i in range(TEST_EPISODES):
             global_state = random.choice(states)
             state = np.array(state_observe(global_state),dtype=np.float32)
-            total_reward = 0.0
-            for j in range(MAX_EP_STEPS):
-                # epsilon-greedy
-                if np.random.uniform() < epsilon:
-                    action = self.ddpg.choose_action(state) #action has been normalized by choos_action()
-                else:
-                    action = normalized([rnd.random() for i in range(self.action_size)])
-                    action = np.array(action)
-
-                # an interesting test, first comment "self.ddpg.learn()" then run the folling code
-                #action = np.array([0.5,0.5,0,0,0,0], dtype=np.float32)
-                #print("Action after adding noise:", action)
-
+            episode_reward = 0.0
+            for j in range(MAX_TEST_STEPS):
+                # Choose the best action by the actor network
+                if mode == "ddpg":
+                    action = self.ddpg.choose_action(state)
+                if mode == "uniform_all":
+                    action = np.array([0.16,0.16,0.16,0.16,0.16,0.16], dtype=np.float32)
+                if mode == "uniform_youngest":
+                    action = np.array([0.0,1.0,0.0,0.0,0.0,0.0], dtype=np.float32)                               
                 (next_global_state, loss) = state_update(global_state, list(action))
                 next_state = np.array(state_observe(next_global_state), dtype=np.float32)
-                reward = -1.0*loss
-
-                self.ddpg.store_transition(state, action, reward, next_state)
-
-                if self.ddpg.pointer > MEMORY_CAPACITY: #and j%5==0:
-                    self.ddpg.learn()
-                    #epsilon = epsilon+0.01 if epsilon < epsilon_max else epsilon_max
                 global_state = next_global_state
                 state = next_state
-                total_reward += reward
-
-                if j == MAX_EP_STEPS-1:
-                    #print('Episode:', i, ' Reward: %i' % int(total_reward), 'Explore: %.5f' % epsilon, )
-                    ave_reward = total_reward/MAX_EP_STEPS
-                    logging.info("Episode {}, Average reward in each step {}".format(i, ave_reward))
-                    rewards.append(ave_reward)
-                    epsilon = epsilon+0.01 if epsilon < epsilon_max else epsilon_max
-                    break
-        print('Running time: ', time.time() - t1)
-        plt.plot(range(MAX_EPISODES), rewards)
-        plt.xlabel('Episode')
-        plt.ylabel('Average reward of each step')
-        plt.show()
+                step_reward = -1.0*loss
+                episode_reward += step_reward
+            episode_reward = episode_reward/MAX_TEST_STEPS
+            logging.info("Episode {}, Average reward in each step {}".format(i, episode_reward))
+            total_reward += episode_reward
+        ave_reward = total_reward/TEST_EPISODES
+        logging.info("Average reward in each step {}".format(ave_reward))
 
 def flatten_lists(lists):
     """
@@ -263,7 +301,7 @@ class DefenderBestResponse:
         agent.learn(Model.State(model),
                         lambda state: flatten_lists(state.N),
                         state_update)
-                        
+
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s / %(levelname)s: %(message)s', level=logging.DEBUG)
     model = test_model()
