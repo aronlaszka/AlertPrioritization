1988f3aad0b67df424d67c7f1b8fd959ee7fcdda
Liang Tong <liangtong39@gmail.com>
2018-08-19 09:23:16 -0500

    Update test.py

--- code/v1/src/test.py
+++ code/v1/src/test.py
@@ -9,15 +9,6 @@ def test_model1():
   Create a very simple test instance of the model (H = 3, |T| = 2, |A| = 2).
   :return: Model object.
   """
-  #alert_types =  [AlertType(1, PoissonDistribution(100), "t1"), 
-  #                AlertType(1, PoissonDistribution(100), "t2"),
-  #                AlertType(1, PoissonDistribution(70), "t3"),
-  #                AlertType(1, PoissonDistribution(90), "t4")]  
-  #attack_types = [AttackType([0, 0.2, 0.4], 100, [0.2, 0.9, 0.1, 0.7], "a1"),
-  #                AttackType([0, 0.8, 1.2], 100, [0.1, 0.7, 0.2, 0.5], "a2"),
-  #                AttackType([0, 0.5, 0.9], 100, [0.1, 0.1, 0.5, 0.5], "a3"),
-  #                AttackType([0, 0.8, 0.9], 100, [0.6, 0.2, 0.1, 0.5], "a4")]
-  
   alert_types =  [AlertType(1, PoissonDistribution(100), "t1"), 
                   AlertType(1, PoissonDistribution(80), "t2")]
   attack_types = [AttackType([0, 0.5, 0.8], 70, [0.2, 0.9], "a1"),
@@ -39,18 +30,20 @@ def test_model2():
   
 def test_model3():
   """
-  Create a very simple test instance of the model (H = 3, |T| = 2, |A| = 2).
+  Create a very simple test instance of the model (H = 3, |T| = 6, |A| = 4).
   :return: Model object.
   """
   alert_types =  [AlertType(1, PoissonDistribution(100), "t1"), 
-                  AlertType(1, PoissonDistribution(100), "t2"),
+                  AlertType(1, PoissonDistribution(90), "t2"),
                   AlertType(1, PoissonDistribution(70), "t3"),
-                  AlertType(1, PoissonDistribution(90), "t4")]  
-  attack_types = [AttackType([0, 0.2, 0.4], 100, [0.2, 0.9, 0.1, 0.7], "a1"),
-                  AttackType([0, 0.8, 1.2], 100, [0.1, 0.7, 0.2, 0.5], "a2"),
-                  AttackType([0, 0.5, 0.9], 100, [0.1, 0.1, 0.5, 0.5], "a3"),
-                  AttackType([0, 0.8, 0.9], 100, [0.6, 0.2, 0.1, 0.5], "a4")]
-  model = Model(3, alert_types, attack_types, 100, 100)
+                  AlertType(1, PoissonDistribution(65), "t4"),
+                  AlertType(1, PoissonDistribution(80), "t4"),
+                  AlertType(1, PoissonDistribution(75), "t4")]  
+  attack_types = [AttackType([0, 0.2, 0.4], 75, [0.2, 0.9, 0.8, 0.1, 0.4, 0.6], "a1"),
+                  AttackType([0, 0.8, 1.2], 100, [0.1, 0.7, 0.2, 0.5, 0.8, 0.6], "a2"),
+                  AttackType([0, 0.5, 0.9], 89, [0.1, 0.1, 0.5, 0.5, 0.5, 0.9], "a3"),
+                  AttackType([0, 0.8, 0.9], 64, [0.6, 0.2, 0.1, 0.5, 0.3, 0.8], "a4")]
+  model = Model(3, alert_types, attack_types, 300, 200)
   return model
 
 def test_defense_action(model, state):
